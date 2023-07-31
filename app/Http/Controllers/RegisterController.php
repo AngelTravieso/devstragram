6733cadd611cf16367ab1c50a2c892de7bb143a5
AngelTravieso <angeltraviesoc@gmail.com>
2023-07-31 19:05:57 -0400

    Controllers en Laravel y convenciones

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -7,6 +7,14 @@ use Illuminate\Http\Request;
 class RegisterController extends Controller
 {
 
+    /**
+     * Los nombres de los métodos son recomendados según los resource controllers
+     * HTTP - MÉTODO - RUTA
+     * GET => index => clientes.index
+     * POST => store => clientes.store
+     * DELETE => destroy => clientes.destroy
+     */
+
     public function index()
     {
         return view('auth.register');
