cb2f82cadbe86901e90ec42f18137a61071f48f7
AngelTravieso <angeltraviesoc@gmail.com>
2023-07-31 18:37:37 -0400

    Creando el formulario de registro en Devstagram

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
                 <h1 class="text-3xl font-black">
                     Devstragram
                 </h1>
-                
+
                 <nav class="flex gap-2 items-center">
                     <a class="font-bold uppercase text-gray-600 text-sm" href="#">Login</a>
                     <a class="font-bold uppercase text-gray-600 text-sm" href="#">Crear Cuenta</a>
@@ -28,8 +28,8 @@
             @yield('contenido')
         </main>
 
-        <footer class="text-center p-5 text-gray-500 font-bold uppercase">
-            DevStagram - Todos los derechos reservados 
+        <footer class="mt-10 text-center p-5 text-gray-500 font-bold uppercase">
+            DevStagram - Todos los derechos reservados
             {{-- @php echo date('Y') @endphp --}}
             {{ now()->year }}
         </footer>
